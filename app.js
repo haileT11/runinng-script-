8327140568be63da249f2815c7fa650400e44442
Michael Tewolde <mtewolde11@gmail.com>
2021-04-10 16:30:05 -0700

    javascript so fun

---
+++ app.js
@@ -0,0 +1,20 @@
+// if (1 === 1){
+ //console.log("It's True!");
+//}
+// Performance Review
+// 3 - superstar
+// 2 - meets expectations
+// 1 - needs improvement
+// 0 - WTF 
+
+let rating = 1
+if (rating === 3){
+    console.log('You are a superstar');
+}
+else if (rating === 2){
+    console.log('meets expectations');
+        
+    }
+else if (rating === 1) {
+    console.log('meets expectations'); 
+}    
---
+++ app2.js
@@ -0,0 +1,29 @@
+//if (1 === 1){
+//  console.log("It's True!");
+//}
+//console.log("3 + 4")
+
+// Performance Review
+// 3 - superstar
+// 2 - meets expectations
+// 1 - needs improvement
+// 0 - WTF 
+
+let rating = 1
+if (rating === 3){
+    console.log('You are a superstar');
+}
+else if (rating === 2){
+     console.log('meets expectations');
+        
+    }
+else if (rating === 1) {
+    console.log('meets expectations'); 
+}
+let age = 30
+ if (age < 6 || age >= 65){
+     console.log('you get in for free');
+ } 
+ else {
+     console.log('you must pay!');
+ }
